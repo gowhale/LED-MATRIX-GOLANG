7ac00783fd09b95bd43f743f1d6236e67b079881
gowhale <gabewhale@gmail.com>
2022-10-12 22:25:41 +0100

    remove heavy gui

--- pkg/gui/gui.go
+++ pkg/gui/gui.go
@@ -4,16 +4,19 @@ package gui
 
 const (
 	//Rows is the amount of rows in the display
-	Rows = 5
+	Rows = 8
 	//Columns is the amount of columns in the display
-	Columns = 30
+	Columns = 8
 	//VapeOn is the value for when a vape light is on
 	VapeOn = 1
 	//VapeOff is the value for when a vape light is off
 	VapeOff = 0
+
+	rowColStartIndex = 0
 )
 
-//Screen represents a screen or hardware visual output
+// Screen represents a screen or hardware visual output
+//
 //go:generate go run github.com/vektra/mockery/cmd/mockery -name Screen -inpkg --filename screen_mock.go
 type Screen interface {
 	VapeLightOn(int) error
