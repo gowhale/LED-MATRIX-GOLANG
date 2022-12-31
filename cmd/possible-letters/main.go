766c1d650723780ac1fcb07ebaaa8fabf49dac3d
gowhale <gabewhale@gmail.com>
2022-12-31 20:15:12 +0000

    feat(diagram): Create a main prog to display letters

---
+++ cmd/possible-letters/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"sort"
+
+	"github.com/gowhale/go-circuit-diagram/pkg/canvas"
+	cc "github.com/gowhale/go-circuit-diagram/pkg/common"
+	"github.com/gowhale/go-circuit-diagram/pkg/components"
+	"github.com/gowhale/led-matrix-golang/pkg/gui"
+)
+
+const (
+	padding = 11
+)
+
+func main() {
+	board := canvas.NewBoard("all-characters", 55, 88, 10)
+	availableChars := []string{}
+	for l := range gui.LetterMap {
+		availableChars = append(availableChars, l)
+	}
+	sort.Strings(availableChars)
+
+	col := 0
+	row := 0
+	for _, l := range availableChars {
+		if col > 0 && col%5 == 0 {
+			row++
+			col = 0
+		}
+		coord := cc.NewCord(5+(col*padding), 5+(row*padding))
+		letterLabel := components.NewLabel(coord, l)
+		board.AddElement(&letterLabel)
+		col++
+	}
+	board.Draw(&cc.OSReal{})
+}
--- go.mod
+++ go.mod
@@ -5,14 +5,15 @@ go 1.17
 require (
 	github.com/gowhale/go-test-data v1.0.0
 	github.com/stianeikeland/go-rpio v4.2.0+incompatible
-	github.com/stretchr/testify v1.7.2
+	github.com/stretchr/testify v1.8.0
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gowhale/go-circuit-diagram v1.0.1 // indirect
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/objx v0.1.1 // indirect
+	github.com/stretchr/objx v0.4.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
